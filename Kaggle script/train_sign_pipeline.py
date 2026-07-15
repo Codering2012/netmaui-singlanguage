36d4ca1c5fd09569453dab7f5637de556248e037
Codering2012 <sucktobeme9009@outlook.com>
2026-07-15 11:50:56 +0700

    Configure workers to isolate GPUs using CUDA_VISIBLE_DEVICES after lazy PyTorch initialization fix

--- Kaggle script/train_sign_pipeline.py
+++ Kaggle script/train_sign_pipeline.py
@@ -1685,8 +1685,7 @@ def _mp_pool_worker_init():
             worker_idx = _mp_gpu_worker_counter.value
             _mp_gpu_worker_counter.value += 1
         gpu_id = worker_idx % get_num_gpus()
-        os.environ["EGL_DEVICE_ID"] = str(gpu_id)
-        os.environ["DRI_PRIME"] = str(gpu_id)
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
 
     try:
         _get_process_extractor(static_mode=False)
