ee17e2cd2db33be1e1f29b7dd5a8f4dc7425b16c
Codering2012 <sucktobeme9009@outlook.com>
2026-07-15 11:30:48 +0700

    Restore EGL GPU delegate access by removing PyTorch CUDA context init from worker startup

--- Kaggle script/train_sign_pipeline.py
+++ Kaggle script/train_sign_pipeline.py
@@ -1674,13 +1674,6 @@ def _mp_pool_worker_init():
             _mp_gpu_worker_counter.value += 1
         gpu_id = worker_idx % NUM_AVAILABLE_GPUS
         os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
-        try:
-            import torch
-
-            if torch.cuda.is_available():
-                torch.cuda.set_device(0)  # Always device 0 inside CUDA_VISIBLE_DEVICES namespace
-        except Exception:
-            pass
 
     try:
         _get_process_extractor(static_mode=False)
